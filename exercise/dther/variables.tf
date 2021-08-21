4ab4529b280db8da35fe7227b0c8cd54bc0a13f0
D Ther <dtherhtun.cw@gmail.com>
2021-08-21 21:36:43 +0630

    add exercise

---
+++ exercise/dther/variables.tf
@@ -0,0 +1,12 @@
+variable "words" {
+	type = object({
+		nouns = list(string),
+		adjectives = list(string),
+		verbs = list(string),
+		adverbs = list(string),
+		numbers = list(number)
+	})
+	description = " A word pool"
+}
+
+
